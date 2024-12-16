f45f51fa53ddd3502313de60c7dbd9f9ac22ead8
PhamQui <20110079@student.hcmute.edu.vn>
2024-12-16 23:24:37 +0700

    push

--- bookstore-server/index.js
+++ bookstore-server/index.js
@@ -12,9 +12,12 @@ const jwt = require("jsonwebtoken");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", 
-    methods: ["GET", "POST"],
-  },
+    origin: [
+      "https://fe-bookstoreshop.vercel.app",
+      "https://fe-bookstoreshop-git-main-quis-projects-845c8459.vercel.app"
+    ],
+    methods: ["GET", "POST"]
+  }
 });
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
@@ -30,8 +33,12 @@ io.on("connection", (socket) => {
 
 // middleware
 app.use(cors({
-  origin: "https://fe-bookstoreshop-git-main-quis-projects-845c8459.vercel.app",
+  origin: [
+    "https://fe-bookstoreshop.vercel.app", 
+    "https://fe-bookstoreshop-git-main-quis-projects-845c8459.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true 
 }));
 
 app.use(express.json());
